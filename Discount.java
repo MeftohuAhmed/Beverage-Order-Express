9185a37ace1484a600769613fa030c70be4eddf8
Ahmed Meftohu <91487090+MeftohuAhmed@users.noreply.github.com>
2023-06-14 13:39:12 -0400

    Update Discount.java
    Added class-level documentation

--- Discount.java
+++ Discount.java
@@ -1,10 +1,25 @@
+/**
+ * The Discount class represents a discount object with a percentage value.
+ * It provides a method to apply the discount to a given amount.
+ */
 public class Discount {
     private double percentage;
 
+    /**
+     * Constructs a new Discount object with the specified percentage value.
+     *
+     * @param percentage The discount percentage value.
+     */
     public Discount(double percentage) {
         this.percentage = percentage;
     }
 
+    /**
+     * Applies the discount to the given amount.
+     *
+     * @param amount The original amount to which the discount is applied.
+     * @return The discounted amount after applying the discount.
+     */
     public double applyDiscount(double amount) {
         return amount * (1 - (percentage / 100));
     }
