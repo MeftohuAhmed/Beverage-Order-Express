61bb5b7fe77be27c0554f77eb40d8937d6f43e64
Ahmed Meftohu <91487090+MeftohuAhmed@users.noreply.github.com>
2023-06-14 13:36:43 -0400

    Update Juice.java
    Added class-level documentation

--- Juice.java
+++ Juice.java
@@ -1,14 +1,33 @@
 import javax.swing.*;
 
+/**
+ * The Juice class represents a specific type of beverage, juice, and extends the Beverage class.
+ * It adds an additional attribute, fruit, to represent the fruit used in the juice.
+ * The class provides a constructor to prompt the user for the fruit, calculates the price based on size and fruit,
+ * and overrides the toString() method to provide a string representation of the juice.
+ */
 public class Juice extends Beverage {
     private String fruit;
     private String[] fruits = {"Apple", "Orange", "Pineapple"};
 
+    /**
+     * Constructs a new Juice object with the specified size, amount, and label component.
+     * Prompts the user to select the fruit used in the juice using a dialog box.
+     * Calculates the price based on the size and fruit.
+     *
+     * @param size The size of the juice (e.g., small, medium, large).
+     * @param amount The number of glasses of juice.
+     * @param lbl The label component used for prompting the user.
+     */
     public Juice(String size, int amount, BeverageShop lbl) {
         super(size, amount);
+
         try {
+            // Prompt the user to select the fruit used in the juice using a dialog box
             fruit = (String) JOptionPane.showInputDialog(lbl, "Select a fruit:", "Fruit Selection",
                     JOptionPane.QUESTION_MESSAGE, null, fruits, fruits[0]);
+
+            // Calculate the price based on size and fruit
             double price;
             if (size.equals("Small"))
                 price = 5.0;
@@ -24,9 +43,17 @@ public class Juice extends Beverage {
 
             setPrice(price);
         } catch (NullPointerException e) {
+            // Handle case when the user cancels the fruit selection dialog
+            // The fruit will be null in this case
         }
     }
 
+    /**
+     * Returns a string representation of the juice.
+     * Appends the fruit to the superclass string representation.
+     *
+     * @return The string representation of the juice.
+     */
     @Override
     public String toString() {
         return super.toString() + fruit + " Juice";
