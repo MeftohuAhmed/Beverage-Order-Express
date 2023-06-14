448325c34f93fc37c80f707c5541011892d9f668
Ahmed Meftohu <91487090+MeftohuAhmed@users.noreply.github.com>
2023-06-14 13:33:01 -0400

    Update Water.java
    Added class-level documentation

--- Water.java
+++ Water.java
@@ -1,17 +1,33 @@
 import javax.swing.*;
 
+/**
+ * The Water class represents a specific type of beverage, water, and extends the Beverage class.
+ * It adds an additional attribute, iced, to represent the ice preference of the water.
+ * The class provides a constructor to prompt the user for the ice preference, calculates the price based on size,
+ * and overrides the toString() method to provide a string representation of the water.
+ */
 public class Water extends Beverage {
     private boolean iced;
 
+    /**
+     * Constructs a new Water object with the specified size, amount, and label component.
+     * Prompts the user to select the ice preference of the water using a dialog box.
+     * Calculates the price based on the size and ice preference.
+     *
+     * @param size The size of the water (e.g., small, medium, large).
+     * @param amount The number of glasses of water.
+     * @param lbl The label component used for prompting the user.
+     */
     public Water(String size, int amount, BeverageShop lbl) {
         super(size, amount);
 
-        // Prompt the user to select ice preference using a dialog box
+        // Prompt the user to select the ice preference using a dialog box
         if (JOptionPane.showConfirmDialog(lbl, "Would you like ice?", "Ice", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
             iced = true;
         else
             iced = false;
 
+        // Calculate the price based on size and ice preference
         double price;
         if (size.equals("Small"))
             price = 1.0;
@@ -26,6 +42,12 @@ public class Water extends Beverage {
         setPrice(price);
     }
 
+    /**
+     * Returns a string representation of the water.
+     * Appends the ice preference to the superclass string representation.
+     *
+     * @return The string representation of the water.
+     */
     @Override
     public String toString() {
         if (iced)
