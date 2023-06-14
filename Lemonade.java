8c47870fb07f0cd94d2a8b952e19e84246555a40
Ahmed Meftohu <91487090+MeftohuAhmed@users.noreply.github.com>
2023-06-14 13:36:16 -0400

    Update Lemonade.java
    Added class-level documentation

--- Lemonade.java
+++ Lemonade.java
@@ -1,25 +1,42 @@
 import javax.swing.*;
 
+/**
+ * The Lemonade class represents a specific type of beverage, lemonade, and extends the Beverage class.
+ * It adds additional attributes, sugar and carbonated, to represent the sugar preference and carbonation level of the lemonade.
+ * The class provides a constructor to prompt the user for the sugar and carbonation preferences,
+ * calculates the price based on size, sugar preference, and carbonation level,
+ * and overrides the toString() method to provide a string representation of the lemonade.
+ */
 public class Lemonade extends Beverage {
     private boolean sugar;
     private boolean carbonated;
 
+    /**
+     * Constructs a new Lemonade object with the specified size, amount, and label component.
+     * Prompts the user to select the sugar preference and carbonation level using dialog boxes.
+     * Calculates the price based on the size, sugar preference, and carbonation level.
+     *
+     * @param size The size of the lemonade (e.g., small, medium, large).
+     * @param amount The number of glasses of lemonade.
+     * @param lbl The label component used for prompting the user.
+     */
     public Lemonade(String size, int amount, BeverageShop lbl) {
         super(size, amount);
 
-        // Prompt the user to select sugar preference using a dialog box
+        // Prompt the user to select the sugar preference using a dialog box
         if (JOptionPane.showConfirmDialog(lbl, "Would you like sugar?", "Sugar", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
             sugar = true;
         else
             sugar = false;
 
-        // Prompt the user to select carbonation preference using a dialog box
+        // Prompt the user to select the carbonation preference using a dialog box
         if (JOptionPane.showConfirmDialog(lbl, "Would you like it carbonated?", "Carbonation",
                 JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION)
             carbonated = true;
         else
             carbonated = false;
 
+        // Calculate the price based on size, sugar preference, and carbonation level
         double price;
         if (size.equals("Small"))
             price = 3.0;
@@ -36,11 +53,17 @@ public class Lemonade extends Beverage {
         setPrice(price);
     }
 
+    /**
+     * Returns a string representation of the lemonade.
+     * Includes information about sugar preference and carbonation level.
+     *
+     * @return The string representation of the lemonade.
+     */
     @Override
     public String toString() {
         String sugarOption = sugar ? "with sugar" : "without sugar";
         String carbonationOption = carbonated ? "carbonated" : "non-carbonated";
 
-        return super.toString() + "Lemonade (" + sugarOption + ", " + carbonationOption + ")";
+        return super.toString() + " Lemonade (" + sugarOption + ", " + carbonationOption + ")";
     }
 }
