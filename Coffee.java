4a3c39b49f6aac8f89981f090624808f0ee05a46
Ahmed Meftohu <91487090+MeftohuAhmed@users.noreply.github.com>
2023-06-14 13:37:46 -0400

    Update Coffee.java
    Added class-level documentation

--- Coffee.java
+++ Coffee.java
@@ -1,8 +1,23 @@
 import javax.swing.*;
 
+/**
+ * The Coffee class represents a specific type of beverage, coffee, and extends the Beverage class.
+ * It adds an additional attribute, milk, to represent the milk preference of the coffee.
+ * The class provides a constructor to prompt the user for the milk preference, calculates the price based on size and milk preference,
+ * and overrides the toString() method to provide a string representation of the coffee.
+ */
 public class Coffee extends Beverage {
     private boolean milk;
 
+    /**
+     * Constructs a new Coffee object with the specified size, amount, and label component.
+     * Prompts the user to select the milk preference using a dialog box.
+     * Calculates the price based on the size and milk preference.
+     *
+     * @param size The size of the coffee (e.g., small, medium, large).
+     * @param amount The number of glasses of coffee.
+     * @param lbl The label component used for prompting the user.
+     */
     public Coffee(String size, int amount, BeverageShop lbl) {
         super(size, amount);
 
@@ -12,6 +27,7 @@ public class Coffee extends Beverage {
         else
             milk = false;
 
+        // Calculate the price based on size and milk preference
         double price;
         if (size.equals("Small"))
             price = 3.5;
@@ -26,6 +42,13 @@ public class Coffee extends Beverage {
         setPrice(price);
     }
 
+    /**
+     * Returns a string representation of the coffee.
+     * If milk preference is true, it appends " Coffee with Milk" to the superclass string representation.
+     * Otherwise, it appends " Coffee".
+     *
+     * @return The string representation of the coffee.
+     */
     @Override
     public String toString() {
         if (milk)
